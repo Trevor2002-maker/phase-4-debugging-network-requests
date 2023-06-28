fdeb6ec5a4b2bd16591bd2b2ebe1faf0f0fa2f9b
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-06-28 09:38:22 +0300

    Done

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -15,19 +15,19 @@ function MovieForm() {
   });
 
   function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/movies", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((r) => r.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }
+  e.preventDefault();
+  fetch("/movies", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
   function handleChange(e) {
     const value =
